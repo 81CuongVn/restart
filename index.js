c53eefd256cdd7673f986f92132c5780f726774c
MoonVNN <60611504+MoonVN571@users.noreply.github.com>
2021-04-09 15:53:59 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-const token = require('dotenv').config();
+require('dotenv').config();
 
 const config = {
     token: process.env.TOKEN,
